e359d30289160c70ad8b6aac2032de80c0854dcf
CristianSilvaGrosseli <cristian23sai@gmail.com>
2025-10-07 15:59:03 -0300

    feature: create Square componente
    This component is a tic-tac-toe ui piece

---
+++ app/components/tic-tac-toe/square/component.tsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+export default function Square({
+  value,
+  onSquareClick
+}: {
+  value: string,
+  onSquareClick: () => void
+})
+{
+  return (
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
