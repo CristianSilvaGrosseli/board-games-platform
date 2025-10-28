93e1dc96d00f8f45141ce1843c7b7882dc7c2f83
Cristian Silva Grosseli <cristian.silva.grosseli@gmail.com>
2025-10-28 17:03:51 -0300

    refactor: move state management to BoardWrapper
    Now the BoardWrapper component is the only one stateful component that manage
    the history and currentMove states.
    TicTacToe and Kalah boards become functional components

--- app/components/board-wrapper/component.tsx
+++ app/components/board-wrapper/component.tsx
@@ -1,11 +1,13 @@
-import { JSX, useRef } from "react";
-import { GameNameEnum } from "@/app/enums/GameNameEnum";
-import TicTacToeUi from "@/app/components/tic-tac-toe/component";
-import KalahBoard from "@/app/components/mancala/kalah-board/component";
-import GameController from "@/app/GameControllers/GameControllerInterface";
+import { JSX, useState, useEffect, useRef } from "react";
 import GameControllerFactory from "@/app/GameControllers/GameControllerFactory";
+import GameController from "@/app/GameControllers/GameControllerInterface";
 import Player from "@/app/Player/Player";
+import IAFactory from "@/app/IA/IAFactory";
+import { GameNameEnum } from "@/app/enums/GameNameEnum";
 import { PlayerTypeEnum } from "@/app/enums/PlayerTypeEnum";
+import { IANameEnum } from "@/app/enums/IANameEnum";
+import TicTacToeBoard from "@/app/components/tic-tac-toe/board/component";
+import KalahBoard from "@/app/components/mancala/kalah-board/component";
 
 export default function BoardWrapper({
   choosedGame
@@ -15,13 +17,14 @@ export default function BoardWrapper({
 })
 {
   const gameControllerRef = useRef<GameController>(null);
+  const player1 = new Player(PlayerTypeEnum.Human, "Cristian", true);
+  const player2 = new Player(PlayerTypeEnum.IA, "Beth Harmon", false);
+  const choosedIA = IANameEnum.MCTS;
 
   const getGamerController = (controllerInitializer: (arg1: Player, arg2: Player) => GameController): GameController =>
   {
     if (gameControllerRef.current === null)
     {
-      const player1 = new Player(PlayerTypeEnum.Human, "Cristian", true);
-      const player2 = new Player(PlayerTypeEnum.IA, "Beth Harmon", false);
       gameControllerRef.current = controllerInitializer(player1, player2);
     }
     if (gameControllerRef.current === null)
@@ -31,22 +34,92 @@ export default function BoardWrapper({
     return gameControllerRef.current;
   }
 
+  let handlePlay: (play: any) => void;
+  const onPlay: (play: any) => void = (play: any) => handlePlay(play);
+
   let boardComponent: JSX.Element;
   if (choosedGame === GameNameEnum.TicTacToe)
   {
-    boardComponent = <TicTacToeUi gameController={getGamerController(GameControllerFactory.CreateTicTacToeControllerInstance)} />
+    boardComponent = <TicTacToeBoard gameController={getGamerController(GameControllerFactory.CreateTicTacToeControllerInstance)} onPlay={onPlay} />
   }
   else if (choosedGame === GameNameEnum.Kalah)
   {
-    boardComponent = <KalahBoard gameController={getGamerController(GameControllerFactory.CreateKalahControllerInstance)} />
+    boardComponent = <KalahBoard gameController={getGamerController(GameControllerFactory.CreateKalahControllerInstance)} onPlay={onPlay} />
   }
   else
   {
     throw `BoardWrapper component: invalid GameNameEnum: ${choosedGame}`;
   }
+
+  const gameController = gameControllerRef.current;
+  if (gameController === null)
+  {
+    throw "BoardWrapper: error to initialize a GameController instance";
+  }
+
+  const [history, setHistory] = useState([gameController.getCurrentGameState()]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  handlePlay = (play: any) => {
+    gameController.addPlay(play);
+    const newState = gameController.getCurrentGameState();
+    const nextHistory = [...history.slice(0, currentMove + 1), newState];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  useEffect(() => {
+    const currentTurnPlayer: Player = gameController.getCurrentTurnPlayer();
+    if (currentTurnPlayer.getType() === PlayerTypeEnum.IA &&
+        !gameController.isGameOver()
+    )
+    {
+      const ia = IAFactory.CreateInstance(choosedIA, gameController);
+      const bestPlay = ia.getBestAction();
+      handlePlay(bestPlay);
+    }
+  }, [history]);
+
+  const jumpTo = (nextMove: number) => {
+    gameController.setCurrentGameState(nextMove);
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0)
+    {
+      description = "Go to move #" + move;
+    }
+    else
+    {
+      description = "Go to game start";
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move) }>{description}</button>
+      </li>
+    );
+  });
+
+  const isGameOver = gameController.isGameOver();
+  let status: string = "";
+  if (isGameOver)
+  {
+    status = gameController.hasWinner() ? `Winner: ${gameController.getWinnerName()}` : "Draw";
+  }
+  else
+  {
+    status = "Next player: " + gameController.getCurrentTurnPlayer().getName();
+  }
+
   return (
     <>
+      <div className='status'>{status}</div>
       {boardComponent}
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </>
   );
 }
--- app/components/tic-tac-toe/board/component.tsx
+++ app/components/tic-tac-toe/board/component.tsx
@@ -1,69 +1,33 @@
 import GameController from '@/app/GameControllers/GameControllerInterface';
-import Square from '../square/component';
+import Square from '@/app/components/tic-tac-toe/square/component';
 import './styles.css'
 
-export default function Board({
-  xIsNext,
+export default function TicTacToeBoard({
   gameController,
   onPlay
 }: {
-  xIsNext: boolean,
   gameController: GameController,
-  onPlay: (square: string[]) => void
+  onPlay: (play: any) => void
 })
 {
   const squares = gameController.getCurrentBoardState();
-  const hasWinner = gameController.hasWinner();
-  const status = hasWinner ? "Winner: " + gameController.getWinnerName() : "Next player: " + (xIsNext ? "X" : "O");
-
-  const onClickHandler = (index: number) => {
-    if (!xIsNext || squares[index] || hasWinner)
-    {
-      return;
-    }
-
-    gameController.addPlay(index);
-    onPlay(gameController.getCurrentBoardState());
-  };
-
   return (
     <>
-      <div className='status'>{status}</div>
       <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => onClickHandler(0)} />
-        <Square value={squares[1]} onSquareClick={() => onClickHandler(1)} />
-        <Square value={squares[2]} onSquareClick={() => onClickHandler(2)} />
+        <Square value={squares[0]} onSquareClick={() => onPlay(0)} />
+        <Square value={squares[1]} onSquareClick={() => onPlay(1)} />
+        <Square value={squares[2]} onSquareClick={() => onPlay(2)} />
       </div>
       <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => onClickHandler(3)} />
-        <Square value={squares[4]} onSquareClick={() => onClickHandler(4)} />
-        <Square value={squares[5]} onSquareClick={() => onClickHandler(5)} />
+        <Square value={squares[3]} onSquareClick={() => onPlay(3)} />
+        <Square value={squares[4]} onSquareClick={() => onPlay(4)} />
+        <Square value={squares[5]} onSquareClick={() => onPlay(5)} />
       </div>
       <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => onClickHandler(6)} />
-        <Square value={squares[7]} onSquareClick={() => onClickHandler(7)} />
-        <Square value={squares[8]} onSquareClick={() => onClickHandler(8)} />
+        <Square value={squares[6]} onSquareClick={() => onPlay(6)} />
+        <Square value={squares[7]} onSquareClick={() => onPlay(7)} />
+        <Square value={squares[8]} onSquareClick={() => onPlay(8)} />
       </div>
     </>
   );
-}
-
-function calculateWinner(squares: string[]) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
